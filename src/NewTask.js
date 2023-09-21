08a03c045d9a0cec28335220fb560f7b27a44928
luismaguilar <laguilar1@murraystate.edu>
2023-09-20 22:20:18 -0500

    Added Basic Form

--- src/NewTask.js
+++ src/NewTask.js
@@ -1,14 +1,17 @@
 import React from "react";
+import './NewTask.css';
 
 function NewTask() {
     return (
-
-
-
-
-
-
-
-        
+        <form>
+            <label>
+                Title: <input name="title"></input>
+            </label>
+            <label>
+                Desciption <input name="title"></input>
+            </label>
+        </form>
     )
 }
+
+export default NewTask;
