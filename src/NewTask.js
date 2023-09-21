4c54a6e62f4d3da373178e8df5fd1fa0209eafc4
luismaguilar <laguilar1@murraystate.edu>
2023-09-20 22:30:31 -0500

    Popup Package Installed

--- src/NewTask.js
+++ src/NewTask.js
@@ -1,17 +1,31 @@
 import React from "react";
 import './NewTask.css';
+import Popup from "reactjs-popup";
 
 function NewTask() {
     return (
-        <form>
-            <label>
-                Title: <input name="title"></input>
-            </label>
-            <label>
-                Desciption <input name="title"></input>
-            </label>
-        </form>
+        <div>
+            <h4>Popup - GeeksforGeeks</h4>
+            <Popup trigger=
+                {<button> Click to open modal </button>}
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                                Welcome to GFG!!!
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close modal
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+        </div>
     )
-}
-
+};
 export default NewTask;
