68d452e2f31513fa639c5bdd37bceefd9a1f86b4
semerson5 <semerson5@murraystate.edu>
2023-09-22 14:12:01 -0500

    Progress on Layout
    Initial layout almost completed.

--- src/TaskList.js
+++ src/TaskList.js
@@ -29,9 +29,13 @@ const handleClick = () => {
                 <h1>Task Manager</h1>
                 <input value={task} onInput={(e) =>setTasks(e.target.value)} />
                 <button type="button" onClick={() => handleClick()}>add</button>
-                
             </div>
+            <h2>Tasks</h2>
             <hr class="solid"></hr>
+            <input type="checkbox"/>
+            <label>Place Holder for Task</label> 
+            <button>Edit</button>
+            <button>Delete</button>
         </>
         
     );
