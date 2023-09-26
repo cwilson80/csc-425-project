a0409cab5bde8b43d52f7b72df0d6a1276e7c88b
luismaguilar <laguilar1@murraystate.edu>
2023-09-26 10:58:20 -0500

    Form Update

--- src/NewTask.js
+++ src/NewTask.js
@@ -5,6 +5,16 @@ import TaskDatePicker from './TaskDatePicker.js';
 
 function NewTask() {
 
+    const [formData, setFormData] = useState({title: "", description: ""});
+
+    const handleChange = (e) => {
+        setFormData(e.target.value);
+   }
+
+    const handleSubmit = (e) => {
+        alert(`Title: ${formData.title}`);
+    }
+
     return (
         <div>
             <Popup trigger=
@@ -13,14 +23,26 @@ function NewTask() {
                 {
                     close => (
                         <div class='modal'>
-                            <div class='content'>
-                                <input id="title" placeholder="Title"></input>
-                                <textarea id="desc" placeholder="Desciprtion"></textarea>
-                            </div>
+                            <form onSubmit={handleSubmit}>
+                                <div class='content'>
+                                    <input 
+                                        id="title" 
+                                        placeholder="Title" 
+                                        value={formData.title}
+                                        onChange={handleChange}    
+                                        />
+                                    <textarea 
+                                        id="desc" 
+                                        placeholder="Description" 
+                                        value={formData.description}
+                                        onChange={handleChange}
+                                        />
+                                </div>
+                                </form>
                             <div>
                                 <TaskDatePicker />
                                 <button type="submit" onClick=
-                                    {() => close()}>
+                                    {handleSubmit}>
                                         Add Task
                                 </button>
                             </div>
