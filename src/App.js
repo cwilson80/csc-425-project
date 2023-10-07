2b3fcdaceebfee9e560469135f7c69d730988519
luismaguilar <laguilar1@murraystate.edu>
2023-10-07 11:46:20 -0500

    Added handleAddTask to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 import TaskList from './TaskList';
+import NewTask from './NewTask';
 
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = (newTask) => {
+    const task = {newTask, id: tasks.length + 1, completed: false}
+    setTasks([...tasks, task]);
+  }
+
+
   return (
     <div className="App">
+      <NewTask onTaskAdd={handleAddTask} />
       <TaskList />
     </div>
   );
