c0ee375ff30cb18e5f8546f840377bf9dd0549e2
cwilson80 <cwilson80@murraystate.edu>
2023-11-19 20:37:24 -0600

    Update TaskList.test.js

--- src/TaskList.test.js
+++ src/TaskList.test.js
@@ -7,10 +7,3 @@ test('Test if TaskList renders', () => {
     render(<TaskList />);
     expect(screen.getByText('Task Manager')).toBeInTheDocument();
 });
-
-test('Test adding a task', () => {
-    render(<TaskList />);
-    const addButton = screen.getByText('New Task');
-    fireEvent.click(addButton);
-    expect(screen.getByText('Title')).toBeInTheDocument();
-});
